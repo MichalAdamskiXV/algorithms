53863053a6daaf17e24e063749c08ac9b6fd8e98
MichalAdamskiXV <madamskixv@gmail.com>
2026-03-22 12:21:31 +0100

    Eklides rekurencyjnie

--- euklides.py
+++ euklides.py
@@ -1,3 +1,5 @@
+# ----ALGORYTMY ITERACYJNE ---- #
+
 #Największy wspólny dzielnik - modulo
 def NWD(a,b):
     while b > 0:
@@ -13,4 +15,19 @@ print(NWD(3,9))
 def NWW(a,b):
     return (a*b) // NWD(a,b)
 
-print(NWD(27,9))
+print(NWW(27,9))
+
+
+# ----ALGORYTMY REKURENCYJNE ---- #
+def NWD_2(a, b):
+    if b == 0:
+        return a
+    else:
+        return NWD_2(b, a % b)
+
+print(NWD_2(3, 27))
+
+def NWW_2(a,b):
+    return (a * b) // NWD_2(a, b)
+
+print(NWW_2(27,9))
